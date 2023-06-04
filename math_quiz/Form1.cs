3e6cc104753347f7dc3aea8e72e0d03d000dd27c
capatabianca <bianca.capata@cncn.ro>
2023-06-04 15:38:27 +0300

    T Rex endless runner game

---
+++ math_quiz/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace math_quiz
+{
+    public partial class Form1 : Form
+    {
+        Random randomizer = new Random();
+        int sumvar1, sumvar2;
+        int diffvar1, diffvar2;
+        int multvar1, multvar2;
+        int subvar1, subvar2;
+        int timeLeft;
+
+       
+
+        public void StartTheQuiz()
+        {
+            sumvar1 = randomizer.Next(51);
+            sumvar2 = randomizer.Next(51);
+            plusLeftLabel.Text = sumvar1.ToString();
+            plusRightLabel.Text = sumvar2.ToString();
+            sum.Value = 0;
+
+            diffvar1 = randomizer.Next(1, 101);
+            diffvar2 = randomizer.Next(1, diffvar1);
+            minusLeftLabel.Text = diffvar1.ToString();
+            minusRightLabel.Text = diffvar2.ToString();
+            difference.Value = 0;
+
+            multvar1 = randomizer.Next(2, 11);
+            multvar2 = randomizer.Next(2, 11);
+            timesLeftLabel.Text = multvar1.ToString();
+            timesRightLabel.Text = multvar2.ToString();
+            product.Value = 0;
+
+            subvar2 = randomizer.Next(2, 11);
+            int tempSth = randomizer.Next(2, 11);
+            subvar1 = subvar2 * tempSth;
+            dividedLeftLabel.Text = subvar1.ToString();
+            dividedRightLabel.Text = subvar2.ToString();
+            quotient.Value = 0;
+
+            timeLeft = 30;
+            timeLabel.Text = "Ai 30 secunde.";
+            timer1.Start();
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            if (CheckIfTrue())
+            {
+                timer1.Stop();
+                MessageBox.Show("Felicitări!");
+                startButton.Enabled = true;
+            }
+            else if (timeLeft > 0)
+            {
+                timeLeft = timeLeft - 1;
+                timeLabel.Text = timeLeft + "seconds";
+            }
+            else
+            {
+                timer1.Stop();
+                timeLabel.Text = "Timpul a expirat!";
+                MessageBox.Show("Încearcă din nou!");
+                sum.Value = sumvar1 + sumvar2;
+                difference.Value = diffvar1 - diffvar2;
+                product.Value = multvar1 * multvar2;
+                quotient.Value = subvar1 / subvar2;
+                startButton.Enabled = true;
+            }
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            StartTheQuiz();
+            startButton.Enabled = false;
+        }
+
+        public bool CheckIfTrue()
+        {
+            if ((sumvar1 + sumvar2 == sum.Value) && (diffvar1 - diffvar2 == difference.Value) && (multvar1 * multvar2 == product.Value) && (subvar1 / subvar2 == quotient.Value))
+            { return true; }
+            else
+            { return false; }
+        }
+
+        public Form1()
+        {
+        InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
---
+++ trex_game/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace trex_game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtScore = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.trex = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trex)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtScore
+            // 
+            this.txtScore.AutoSize = true;
+            this.txtScore.Font = new System.Drawing.Font("Consolas", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtScore.Location = new System.Drawing.Point(12, 9);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(135, 33);
+            this.txtScore.TabIndex = 2;
+            this.txtScore.Text = "Score: 0";
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.MainGameTimerEvent);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.Location = new System.Drawing.Point(-3, 404);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(807, 50);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::trex_game.Properties.Resources.obstacle_2_1_;
+            this.pictureBox4.Location = new System.Drawing.Point(656, 371);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(32, 33);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox4.TabIndex = 1;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Tag = "obstacles";
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::trex_game.Properties.Resources.obstacle_1_1_;
+            this.pictureBox3.Location = new System.Drawing.Point(485, 358);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(23, 46);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox3.TabIndex = 1;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Tag = "obstacles";
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // trex
+            // 
+            this.trex.Image = global::trex_game.Properties.Resources.running_1_;
+            this.trex.Location = new System.Drawing.Point(92, 367);
+            this.trex.Name = "trex";
+            this.trex.Size = new System.Drawing.Size(40, 43);
+            this.trex.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.trex.TabIndex = 1;
+            this.trex.TabStop = false;
+            this.trex.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.trex);
+            this.Controls.Add(this.txtScore);
+            this.Name = "Form1";
+            this.Text = "T Rex endless runner game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyisup);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trex)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox trex;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label txtScore;
+        private System.Windows.Forms.Timer gameTimer;
+    }
+}
+
